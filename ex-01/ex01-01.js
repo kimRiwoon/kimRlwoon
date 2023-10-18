417dcb64d876518c33c0a2f34e79091d22102482
kimRlwoon <qhdekf2161@naver.com>
2023-10-18 12:52:01 +0900

    javascript .html

---
+++ ex-01/ex01-01.js
@@ -0,0 +1,10 @@
+var 사과 = 2500; //let,const,선언
+var 바나나 = 4000;
+var 배 = 3000;
+var 키위 = 900;
+var 거스름돈 = 2000;
+var 총합 = 사과 + 바나나 + 배 + 키위;
+
+console.log(총합 - 키위);
+console.log(총합);
+console.log(총합 - 20000);
